f92a60f30ab062be50039fac98d4a44839bbc9b5
Svyatoslau <svyatyar2@gmail.com>
2021-03-28 22:20:56 +0300

    feat: add solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  try {
+    if(!Array.isArray(arr)) throw new Error;
+  } catch (error) {
+    console.log(error);
+  }
+  let map = new Map([["--discard-next",0],["--discard-prev",1],["--double-next",2],["--double-prev",3]]);
+  let array = new Array();
+  let hasPre=true;
+  for(let i=0;i<arr.length;i++){
+    
+      switch (map.get(arr[i])) {
+        case 0:{
+          if(typeof arr[i+1] === 'number'){
+            hasPre=false;
+            i++;
+          }
+          break;
+        }
+        case 1:{
+          if(hasPre && typeof arr[i-1] === 'number'){
+            array.pop();
+          }
+          break;
+        }
+        case 2:{
+          if(typeof arr[i+1] === 'number'){
+            array.push(arr[i+1]);
+            array.push(arr[i+1]);
+            i++;
+          }
+          break;
+        }
+        case 3:{
+          if(hasPre && typeof arr[i-1] === 'number'){
+            array.push(arr[i-1]);
+            array.push(arr[i-1]);
+          }
+          break;
+        }
+        default:
+          if(!typeof arr[i] === 'number') break;
+          array.push(arr[i]);
+          hasPre=true;
+          break;
+      }
+    
+  }
+  return array;
 };
