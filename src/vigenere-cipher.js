20496eef71aae6eca0747c093845f80822a21bb4
Svyatoslau <svyatyar2@gmail.com>
2021-03-30 01:34:15 +0300

    feat: add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(flag = true) {
+    this.flag = flag;
+  }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) return Error;
+
+    let nMessage = String(message);
+    let nKey = String(key);
+    nKey = ''.padStart(nMessage.length, nKey);
+    let start = 'A'.charCodeAt(0);
+    let str = ''
+    for (let i = 0, k = 0; i < nMessage.length; i++, k++) {
+      if (/[A-Za-z]/.test(nMessage[i])) {
+        let M = nMessage[i].toUpperCase();
+        M = Number(M.charCodeAt(0));
+        let K = nKey[k].toUpperCase();
+        K = Number(K.charCodeAt(0));
+        let ind = ((M - start) + (K - start)) % 26;
+        str += String.fromCharCode(ind + start);
+      } else {
+        k--;
+        str += nMessage[i];
+      }
+    }
+    if (!this.flag) return str.split('').reverse().join('');
+    return str;
+
+
+  }
+  decrypt(message = -1, key = -1) {
+    if (message === undefined || key === undefined) return Error;
+    let nShifr = String(message);
+    let nKey = String(key);
+    nKey = ''.padStart(nMessage.length, nKey);
+    let start = 'A'.charCodeAt(0);
+    let str = ''
+    for (let i = 0, k = 0; i < nShifr.length; i++, k++) {
+      if (/[A-Z]/.test(nShifr[i])) {
+        let ind = ((nShifr[i].toUpperCase().charCodeAt(0) - start) - (nKey[k].toUpperCase().charCodeAt(0) - start) + 26) % 26;
+        str += String.fromCharCode(ind);
+      } else {
+        k--;
+        str += nMessage[i];
+      }
+    }
+    if (!this.flag) return str.split('').reverse().join('');
+    return str;
+
+
   }
 }
 
